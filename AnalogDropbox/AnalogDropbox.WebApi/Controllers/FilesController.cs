6c50cb3004218cf4a706905de560d4f81cad7559
Rustam Shayahmetov "Credinform <r.shayahmetov@CRREF.local>
2017-05-25 11:09:49 +0300

    WebApi is created

---
+++ AnalogDropbox/AnalogDropbox.WebApi/Controllers/FilesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AnalogDropbox.DataAccess;
+using AnalogDropbox.DataAccess.Sql;
+using AnalogDropbox.Model;
+
+namespace AnalogDropbox.WebApi.Controllers
+{
+    public class FilesController : ApiController
+    {
+        private readonly string ConnectionString;
+        private readonly IUsersRepository _usersRepository;
+
+        public FilesController(string connectionString, IUsersRepository usersRepository)
+        {
+            ConnectionString = connectionString;
+            _usersRepository = usersRepository;
+        }
+
+    }
+}
---
+++ AnalogDropbox/AnalogDropbox.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AnalogDropbox.DataAccess;
+using AnalogDropbox.DataAccess.Sql;
+using AnalogDropbox.Model;
+
+namespace AnalogDropbox.WebApi.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private const string ConnectionString = "Data Source=SQLEXPRESS2016;Initial Catalog=DropboxAnalog;Integrated Security=False;User ID=dropboxuser; Password=dropboxuser";
+        private IUsersRepository _usersRepository = new UsersRepository(ConnectionString);
+        private IFilesRepository _filesRepository;
+
+        public UsersController()
+        {
+            _filesRepository = new FilesRepository(ConnectionString, _usersRepository);
+        }
+
+
+    }
+}
